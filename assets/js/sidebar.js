ff9a87aedbae8d75cacdcf764873af92f74763e6
Min <limminxuan14@gmail.com>
2021-08-03 16:47:54 +0800

    Updates

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,20 +1,22 @@
 function responsive() {
-    if (sidebar.classList.contains("-translate-x-full")) {
-        const btn = document.querySelector(".mobile-menu-button");
-        const sidebar = document.querySelector(".sidebar");
-        
-        btn.addEventListener("click", () => {
-            sidebar.classList.toggle("-translate-x-full");
+    const btn = document.querySelector(".mobile-menu-button");
+    const sidebar = document.querySelector(".sidebar");
+    
+    btn.addEventListener("click", () => {
+        sidebar.classList.toggle("-translate-x-full");
     })
 }
 
-window.addEventListener("load", () => {
-    setTimeout(function update() {
-        responsive();
-    }, 300)
-});
+var stopChange = false;
 
 window.addEventListener("hashchange", () => {
     responsive();
-    console.log('hash');
+    stopChange = true;
 });
+
+window.addEventListener("load", () => {
+    if (!stopChange) {
+        responsive();
+    }
+});
+
